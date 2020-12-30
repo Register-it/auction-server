373f5c9e5d3d0120d8dbd9a56b91ff0508ffacb1
Tiziano Pessa <tiziano.pessa@register.it>
2020-12-30 18:50:00 +0100

    feat: collegato al server Postgres

---
+++ src/main/java/it/register/edu/auction/config/DatabaseConfig.java
@@ -0,0 +1,29 @@
+package it.register.edu.auction.config;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import javax.sql.DataSource;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatabaseConfig {
+
+  @Bean
+  public DataSource getDataSource() throws URISyntaxException {
+
+    URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+    String username = dbUri.getUserInfo().split(":")[0];
+    String password = dbUri.getUserInfo().split(":")[1];
+    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+    DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+    dataSourceBuilder.url(dbUrl);
+    dataSourceBuilder.username(username);
+    dataSourceBuilder.password(password);
+
+    return dataSourceBuilder.build();
+  }
+}
