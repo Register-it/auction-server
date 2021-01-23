812bf78f9beba6e284bbaf23065f14d2bbfa14ef
Spyna <iam.lorenzo.spinelli@gmai.com>
2021-01-23 15:08:18 +0100

    fix: cookies

--- src/main/java/it/register/edu/auction/config/WebConfiguration.java
+++ src/main/java/it/register/edu/auction/config/WebConfiguration.java
@@ -1,5 +1,9 @@
 package it.register.edu.auction.config;
 
+import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
+import org.apache.tomcat.util.http.SameSiteCookies;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,4 +16,12 @@ public class WebConfiguration implements WebMvcConfigurer {
     registry.addRedirectViewController("/", "/playground");
   }
 
+  @Bean
+  public TomcatContextCustomizer sameSiteCookiesConfig() {
+    return context -> {
+      final Rfc6265CookieProcessor cookieProcessor = new Rfc6265CookieProcessor();
+      cookieProcessor.setSameSiteCookies(SameSiteCookies.NONE.getValue());
+      context.setCookieProcessor(cookieProcessor);
+    };
+  }
 }
